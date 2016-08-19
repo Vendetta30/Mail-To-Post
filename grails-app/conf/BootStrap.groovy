696c83af1ac7325c6bed69a498cc35924b5b4ee9
shipra <shipra@nexthoughts.com>
2016-08-19 10:16:51 -0400

    modified bootstrap service code

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,36 +1,13 @@
-import com.springSecurity.Role
-import com.springSecurity.User
-import com.springSecurity.UserRole
+
 
 class BootStrap {
+    def bootstrapService
 
     def init = { servletContext ->
-        if (!(Role.findAllByAuthority("ROLE_ADMIN"))) {
-            User userAdmin = new User(username: "admin", email: "admin@gamil.com", password: "123456", firstName: "admin_first", lastName: "admin_last")
-            userAdmin.save(flush: true, failOnError: true)
 
-            Role roleAdmin = new Role(authority: "ROLE_ADMIN").save(flush: true, failOnError: true)
-
-            Role roleSubAdmin = new Role(authority: "ROLE_SUBADMIN").save(flush: true, failOnError: true)
-
-            Role roleUser = new Role(authority: "ROLE_USER").save(flush: true, failOnError: true)
-
-            UserRole.create(userAdmin, roleAdmin, true)
-            UserRole.create(userAdmin, roleUser, true)
-            UserRole.create(userAdmin, roleSubAdmin, true)
-
-        }
-        Role roleSubAdmin
-        if (!(Role.findAllByAuthority("ROLE_SUBADMIN"))) {
-            roleSubAdmin = new Role(authority: "ROLE_SUBADMIN").save(flush: true, failOnError: true)
-        } else {
-            roleSubAdmin = Role.findByAuthority("ROLE_SUBADMIN")
-
-        }
-        User userSubAdmin = new User(username: "subadmin", email: "subadmin@gmail.com", password: "123456", firstName: "subadmin_first", lastName: "subadmin_last")
-        userSubAdmin.save(flush: true, failOnError: true)
-
-        UserRole.create(userSubAdmin, roleSubAdmin, true)
+        bootstrapService.createRoles()
+        bootstrapService.createAdmins()
+        bootstrapService.createSubadmin()
 
     }
     def destroy = {
