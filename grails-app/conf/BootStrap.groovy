ff9889a0445f3e7938dd85ac494332cd85706fbc
Vijay <vshukla684@gmail.com>
2016-08-24 12:00:47 +0530

    fix merge conflicts when merge vijay

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -4,14 +4,9 @@ class BootStrap {
     def bootstrapService
 
     def init = { servletContext ->
-        Role roleAdmin = Role.findByAuthority("ROLE_ADMIN") ?: new Role(authority: "ROLE_ADMIN").save(flush: true, failOnError: true)
-        Role roleSubAdmin = Role.findByAuthority("ROLE_SUBADMIN") ?: new Role(authority: "ROLE_SUBADMIN").save(flush: true, failOnError: true)
-        Role roleUser = Role.findByAuthority("ROLE_USER") ?: new Role(authority: "ROLE_USER").save(flush: true, failOnError: true)
-
         bootstrapService.createRoles()
-        bootstrapService.createAdmins()
-        bootstrapService.createSubadmin()
-
+        bootstrapService.createAdmin()
+        bootstrapService.createSubAdmin()
     }
     def destroy = {
     }
--- grails-app/services/com/BootstrapService.groovy
+++ grails-app/services/com/BootstrapService.groovy
@@ -9,23 +9,17 @@ import grails.transaction.Transactional
 class BootstrapService {
 
     public void createRoles() {
-        if (Role.count < 1) {
-            new Role(authority: "ROLE_ADMIN").save(flush: true, failOnError: true)
-            new Role(authority: "ROLE_SUBADMIN").save(flush: true, failOnError: true)
-            new Role(authority: "ROLE_USER").save(flush: true, failOnError: true)
-
-        }
-
-
+        Role.findByAuthority("ROLE_ADMIN") ?: new Role(authority: "ROLE_ADMIN").save(flush: true, failOnError: true)
+        Role.findByAuthority("ROLE_SUBADMIN") ?: new Role(authority: "ROLE_SUBADMIN").save(flush: true, failOnError: true)
+        Role.findByAuthority("ROLE_USER") ?: new Role(authority: "ROLE_USER").save(flush: true, failOnError: true)
     }
 
-    public void createAdmins() {
+    public void createAdmin() {
         Role adminRole = Role.findByAuthority("ROLE_ADMIN")
         Role subAdminRole = Role.findByAuthority("ROLE_SUBADMIN")
         Role userRole = Role.findByAuthority("ROLE_USER")
         if (!User.findByUsername("admin")) {
-            User userAdmin = new User(username: "admin", email: "admin@gmail.com", password: "123456",
-                    firstName: "admin_first", lastName: "admin_last")
+            User userAdmin = new User(username: "admin@gmail.com", password: "123456", firstName: "admin_first", lastName: "admin_last")
             userAdmin.save(flush: true, failOnError: true)
             UserRole.create(userAdmin, adminRole, true)
             UserRole.create(userAdmin, userRole, true)
@@ -33,14 +27,12 @@ class BootstrapService {
         }
     }
 
-    public void createSubadmin() {
+    public void createSubAdmin() {
         Role subAdminRole = Role.findByAuthority("ROLE_SUBADMIN")
         if (!User.findByUsername("subadmin")) {
-            User userSubAdmin = new User(username: "subadmin", email: "subadmin@gmail.com", password: "123456",
-                    firstName: "subadmin_first", lastName: "subadmin_last")
+            User userSubAdmin = new User(username: "subadmin@gmail.com", password: "123456", firstName: "subadmin_first", lastName: "subadmin_last")
             userSubAdmin.save(flush: true, failOnError: true)
             UserRole.create(userSubAdmin, subAdminRole, true)
         }
-
     }
 }
