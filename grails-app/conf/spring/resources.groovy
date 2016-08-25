903b9931c4775d5f10ef3e077a81e473d5f62c5f
Vijay <vshukla684@gmail.com>
2016-08-25 16:15:36 +0530

    Normal fix

--- grails-app/conf/spring/resources.groovy
+++ grails-app/conf/spring/resources.groovy
@@ -1,8 +1,20 @@
 // Place your Spring DSL code here
 import grails.plugin.springsecurity.SpringSecurityUtils
+import grails.plugin.springsecurity.web.authentication.AjaxAwareAuthenticationSuccessHandler
 
 beans = {
     def conf = SpringSecurityUtils.securityConfig
+
+    authenticationSuccessHandler(AjaxAwareAuthenticationSuccessHandler) {
+        //borrowed from DefaultSecurityConfig.groovy
+        requestCache = ref('requestCache')
+        defaultTargetUrl = conf.successHandler.defaultTargetUrl // '/'
+        alwaysUseDefaultTargetUrl = conf.successHandler.alwaysUseDefault // false
+        targetUrlParameter = conf.successHandler.targetUrlParameter // 'spring-security-redirect'
+        ajaxSuccessUrl = conf.successHandler.ajaxSuccessUrl // '/login/ajaxSuccess'
+        useReferer = conf.successHandler.useReferer // false
+        redirectStrategy = ref('ajaxSuccessAwareRedirectStrategy')
+    }
 
     ajaxSuccessAwareRedirectStrategy(AjaxSuccessAwareRedirectStrategy) {
         contextRelative = conf.redirectStrategy.contextRelative
